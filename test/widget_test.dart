e18cc585d796f462c6e86baf7bb12192bfc5c947
S Praveen <108536707+Spraveen8-chary@users.noreply.github.com>
2025-09-21 15:17:12 +0530

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:universal_milk/main.dart';
+import 'package:universal_milk/app/app.locator.dart';
+import 'package:universal_milk/features/app/app_view.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  setUpAll(() async {
+    await setupLocator();
+  });
+
+  testWidgets('App builds without errors', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppView());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the app builds without throwing any errors
+    expect(find.byType(AppView), findsOneWidget);
   });
 }
